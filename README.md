cc30aca3232d676d6801814c01a7610720351f3b
Jon Williams <jon@jonwillia.ms>
2022-02-04 19:35:02 -0500

    update godoc

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Ordered Select
 
-This implements [deterministic select](https://www.sethvargo.com/what-id-like-to-see-in-go-2/#deterministic-select) for go1.18+.
+This implements [deterministic `select`](https://www.sethvargo.com/what-id-like-to-see-in-go-2/#deterministic-select) for go1.18+.
 Two to 9 channels, plus a default case are suppported. It utilizes the AST for code generation & 1.18 generics to allow reading from
 arbitrary channel types.
 
--- doc.go
+++ doc.go
@@ -1,6 +1,65 @@
+//go:generate go run ./cmd/generate/... select_gen.go
+
 /*
-Godoc
+	Package oselect allows deterministic ordering for case labels in select statements.
+	For example this contrived example of trying to prioritize ui events in a loop:
+
+		func busyLoop(ctx context.Context, uiMessages, ircMessages, twitterMessages chan <-any) {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+				select {
+				case <-ctx.Done():
+					return
+				case msg := <-uiMessages:
+					dispatchEvent(msg)
+					continue
+				default:
+				}
+				select {
+				case <-ctx.Done():
+					return
+				case msg := <-uiMessages:
+					dispatchEvent(msg)
+					continue
+				case msg := <-ircMessages:
+					dispatchIRC(msg)
+					continue
+				default:
+				}
+				select {
+				case <-ctx.Done():
+					return
+				case msg := <-uiMessages:
+					dispatchEvent(msg)
+				case msg := <-ircMessages:
+					dispatchIRC(msg)
+				case msg := <-twitterMessages:
+					dispatchTwitter(msg)
+				}
+			}
+		}
+
+	becomes:
+
+		func busyLoop(ctx context.Context, uiMessages, ircMessages, twitterMessages chan <-any) {
+			for {
+				var done bool
+				Select4(
+					ctx.Done(), func() { done = true },
+					uiMessages, dispatchEvent,
+					ircMessages, uiMessages,
+					twitterMessages, dispatchTwitter,
+				)
+				if done {
+					return
+				}
+			}
+		}
+
+	Whether or not this is useful is left as an exercise to the reader.
 */
 package oselect
-
-//go:generate go run ./cmd/generate/... select_gen.go
